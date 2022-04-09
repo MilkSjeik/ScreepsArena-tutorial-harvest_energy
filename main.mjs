09609f98e48546461b186f4ec1282c8272d436a3
Stijn Mertens <stijn.mertens@expertum.net>
2022-04-09 17:41:24 +0200

    Initial solution

--- main.mjs
+++ main.mjs
@@ -1,8 +1,24 @@
-import { } from '/game/utils';
-import { } from '/game/prototypes';
-import { } from '/game/constants';
+import { getObjectsByPrototype, findClosestByPath } from '/game/utils';
+import { StructureSpawn, Creep, Source} from '/game/prototypes';
+import { RESOURCE_ENERGY, ERR_NOT_IN_RANGE} from '/game/constants';
 import { } from '/arena';
 
 export function loop() {
-    // Your code goes here
+    const spawn = getObjectsByPrototype(StructureSpawn).find(struct => struct.my);
+    const myCreeps = getObjectsByPrototype(Creep).filter(creep => creep.my);
+    const sources =  getObjectsByPrototype(Source);
+
+    myCreeps.forEach(myCreep => {
+        if(myCreep.store.getFreeCapacity(RESOURCE_ENERGY)){
+            // find closest source
+            const source = findClosestByPath(myCreep, sources);
+            if(myCreep.harvest(source) == ERR_NOT_IN_RANGE){
+                myCreep.moveTo(source);
+            }
+        } else {
+            if(myCreep.transfer(spawn, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                myCreep.moveTo(spawn);
+            }
+        }
+    });
 }
